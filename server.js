3f5523a0f40c79f8131009efc0fb6fe03fe41295
Sashiy1 <aleksander.paluch@wp.pl>
2024-08-06 15:08:49 +0200

    express server added

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+// import "./routes/db.js";
+// import path from "node:path"
+
+// import contactsRouter from "./routes/contactsRouter.js";
+// import usersRouter from "./routes/usersRouter.js";
+// import { auth } from "./middlewares/auth.js";
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/contacts", auth, contactsRouter);
+// app.use("/api/users", usersRouter);
+// app.use("/users", usersRouter);
+// app.use("/avatars", express.static(path.resolve("public/avatars")))
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+});
